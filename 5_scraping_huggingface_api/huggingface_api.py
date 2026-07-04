f382dcbae98d70530b6ded08d4acefa24a9e1609
dophat1 <dophat2403@gmail.com>
2026-07-05 00:25:14 +0200

    print out process and save to csv file

--- 5_scraping_huggingface_api/huggingface_api.py
+++ 5_scraping_huggingface_api/huggingface_api.py
@@ -1,7 +1,7 @@
 import os
 from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
-
+import pandas as pd 
 
 # Load token from .env file
 load_dotenv()
@@ -20,12 +20,24 @@ with open('sample_prompts.txt', 'r') as f:
     prompts = [prompt.strip() for prompt in f]
 
 
-for prompt in prompts:
+answers = {}
+total = len(prompts)
+
+for i, prompt in enumerate(prompts, start=1):
+    print(f"----- Processing {i}/{total} -----")
+    print(f"Prompt: {prompt}")
     messages = [
         {'role': 'system', 'content':'You are an expert for Machine Learning, NLP and Python coding'},
-        {'role': 'user', 'content':prompt}
+        {'role': 'user', 'content': prompt}
     ]
     
-    response = client.chat_completion(messages=messages, max_tokens=500, top_p=0.2)
+    response = client.chat_completion(messages=messages, max_tokens=200, top_p=0.2)
+    print(f"Response: {response.choices[0].message.content}")
+    answers[prompt] = response.choices[0].message.content
+    
 
-    print(response.choices[0].message.content)
+
+
+df = pd.DataFrame(list(answers.items()), columns=['prompt', 'answer'])
+df.to_csv("prompts_answers.csv")
+print("Saved successfully")
