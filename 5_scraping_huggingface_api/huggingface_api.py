cbac9907c1eb2e771af1e155bd475eaa60d9cb58
dophat1 <dophat2403@gmail.com>
2026-07-04 23:56:13 +0200

    add loop so the script can run multiple prompts from a text file

--- 5_scraping_huggingface_api/huggingface_api.py
+++ 5_scraping_huggingface_api/huggingface_api.py
@@ -2,22 +2,30 @@ import os
 from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
 
+
+# Load token from .env file
 load_dotenv()
 
 HF_TOKEN = os.environ['HF_TOKEN']
 
+
+# Create the model
 client = InferenceClient(
     model="meta-llama/Meta-Llama-3-8B-Instruct",
     token=HF_TOKEN
 )
 
-
-messages = [
-    {'role': 'system', 'content':'You are an expert for Machine Learning, NLP and Python coding'},
-    {'role': 'user', 'content':'Explain to me what an API is.'}
-]
+# Separate the .txt file into list of prompts
+with open('sample_prompts.txt', 'r') as f:
+    prompts = [prompt.strip() for prompt in f]
 
 
-response = client.chat_completion(messages=messages, max_tokens=500, top_p=0.2)
+for prompt in prompts:
+    messages = [
+        {'role': 'system', 'content':'You are an expert for Machine Learning, NLP and Python coding'},
+        {'role': 'user', 'content':prompt}
+    ]
+    
+    response = client.chat_completion(messages=messages, max_tokens=500, top_p=0.2)
 
-print(response.choices[0].message.content)
+    print(response.choices[0].message.content)
